189c75011783f5604d2c728ba110824503b9ece5
YannickWnz <yannickwnz@gmail.com>
2024-10-08 11:23:00 +0100

    app name changed

--- app/(full-page)/auth/login/page.tsx
+++ app/(full-page)/auth/login/page.tsx
@@ -56,7 +56,7 @@ const LoginPage = () => {
                 {/* <img src={`/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Sakai logo" className="mb-5 w-6rem flex-shrink-0" /> */}
                 <div 
                 className="flex align-center"
-                style={{width: '200px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+                style={{width: '400px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}
                 >
                     <img
                     src={`/layout/images/logo_itim.png`}
@@ -66,7 +66,7 @@ const LoginPage = () => {
                     />
                     <span
                     style={{fontWeight: "bold", color: "#0003B4", marginBottom: "40px", marginLeft: "10px", fontSize: "2rem"}}
-                    >SYSGHECO {/* Systeme de Gestion des Honoraires et Ecolages */ }</span>
+                    >SIREF {/* Systeme de Gestion des Honoraires et Ecolages */ }</span>
                 </div>
                 <div
                     style={{
