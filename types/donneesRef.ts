003e84dd99c122cc30b60729415553388881af66
YannickWnz <yannickwnz@gmail.com>
2024-10-21 15:57:55 +0100

    donnes ref types files

---
+++ types/donneesRef.ts
@@ -0,0 +1,18 @@
+
+
+
+export type fetchedDonneesRefsDataStructure = {
+    aacCode: number
+    aacCreerPar: string,
+    aacLib: string,
+    aacModifierPar: string, 
+    aacStatus: boolean,
+    proCode: number,
+    proLib: string,
+    filLib: string,
+    filCode: number,
+    nivCode: number,
+    nivLib: string,
+    claLib: string,
+    claCode: number
+}
